993ddf5bc6c635490ebb06f5914030005b75db0f
localhostwiththemost <jpowelldev@gmail.com>
2022-10-12 18:14:05 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const searchBar = document.getElementById("search-bar");
+
+/*
+fetch("http://www.omdbapi.com/?apikey=9a98a936&type=movie")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+*/
