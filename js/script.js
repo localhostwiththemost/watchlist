e934f989253f7471e1a2fb888d4c16ff07645f10
localhostwiththemost <jpowelldev@gmail.com>
2022-10-13 18:24:11 -0500

    Added fetch request to OMDB api on search button click that displays movie data to user when resolved. Also css styling throughout

--- js/script.js
+++ js/script.js
@@ -1,9 +1,52 @@
 "use strict";
 
-const searchBar = document.getElementById("search-bar");
+// 1. Search bar calls to api with the movie title input and displays search results
 
-/*
-fetch("http://www.omdbapi.com/?apikey=9a98a936&type=movie")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
-*/
+// 2."Add to watchlist" btn saves that data to local storage
+
+document.addEventListener("DOMContentLoaded", function () {
+  const searchBar = document.getElementById("search-bar");
+  const btnSearch = document.getElementById("btn-search");
+  const searchResults = document.getElementById("search-results");
+
+  btnSearch.addEventListener("click", function (e) {
+    // fetch the movie based on the title the user input into the searchBar
+
+    e.preventDefault();
+
+    fetch(
+      `http://www.omdbapi.com/?apikey=9a98a936&t=${searchBar.value}&type=movie`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+
+        searchResults.innerHTML = `
+    <div class="movie-card">
+    <div class="poster-container">
+    <img src=${data.Poster} />
+    </div>
+    
+    <div class="info-container">
+    
+    <div class="title-rating-container">
+    <h3>${data.Title}</h3>
+    <h4>&#11088; ${data.Ratings.Value ? data.Ratings[0].Value : "N/A"}</h4>
+    </div>
+
+    <div class="runtime-genre-container">
+    <h5>${data.Runtime}</h5>
+    <h5>${data.Genre}</h5>
+    <div class="addto-watchlist">
+    <img src="../images/add.png" />
+    <h5>Watchlist</h5>
+    </div>
+    </div>
+    
+    <p class="plot">${data.Plot}</p>
+
+    </div>
+    `;
+      });
+  });
+});
