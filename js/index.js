79f4379d1affdc765a9d69f53464980db3af2d0f
localhostwiththemost <jpowelldev@gmail.com>
2022-10-15 18:21:06 -0500

    Movie data is now saved to localStorage when added to watchlist and then a movie card holding that data is created on the Watchlist page

--- js/index.js
+++ js/index.js
@@ -1,36 +1,23 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function () {
-  const searchBar = document.getElementById("search-bar");
-  const btnSearch = document.getElementById("btn-search");
-  const searchResults = document.getElementById("search-results");
+const searchBar = document.getElementById("search-bar");
+const btnSearch = document.getElementById("btn-search");
+const searchResults = document.getElementById("search-results");
 
-  btnSearch.addEventListener("click", function (e) {
-    // fetch the movie based on the title the user input into the searchBar
+// SEARCH BUTTON
+btnSearch.addEventListener("click", function (e) {
+  // fetch the movie based on the title the user input into the searchBar
 
-    e.preventDefault();
+  e.preventDefault();
 
-    fetch(
-      `http://www.omdbapi.com/?apikey=9a98a936&t=${searchBar.value}&type=movie`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+  fetch(
+    `http://www.omdbapi.com/?apikey=9a98a936&t=${searchBar.value}&type=movie`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
 
-        // Save movie data to localStorage when addto-watchlist div is clicked
-        const saveToLS = function (data) {
-          localStorage.setItem("poster", data.Poster);
-          localStorage.setItem("title", data.Title);
-          localStorage.setItem(
-            "rating",
-            data.Ratings.Value ? data.Ratings[0].Value : "N/A"
-          );
-          localStorage.setItem("runtime", data.Runtime);
-          localStorage.setItem("genre", data.Genre);
-          console.log(localStorage);
-        };
-
-        const movieCard = `
+      const movieCard = `
     <div class="movie-card">
     <div class="poster-container">
     <img src=${data.Poster} />
@@ -40,13 +27,13 @@ document.addEventListener("DOMContentLoaded", function () {
     
     <div class="title-rating-container">
     <h3>${data.Title}</h3>
-    <h4>&#11088; ${data.Ratings.Value ? data.Ratings[0].Value : "N/A"}</h4>
+    <h4>&#11088; ${data.Ratings[0].Value ? data.Ratings[0].Value : "N/A"}</h4>
     </div>
 
     <div class="runtime-genre-container">
     <h5>${data.Runtime}</h5>
     <h5>${data.Genre}</h5>
-    <div class="addto-watchlist" onclick="${saveToLS(data)}">
+    <div class="addto-watchlist">
     <img src="../images/add.png" />
     <h5>Watchlist</h5>
     </div>
@@ -57,10 +44,32 @@ document.addEventListener("DOMContentLoaded", function () {
     </div>
     `;
 
-        searchResults.innerHTML = movieCard;
-      })
-      .catch((err) => {
-        searchResults.innerHTML = `<h2 class="error-text">Unable to find what you're looking for. Please try another search.</h2>`;
-      });
-  });
+      // Save movie data to localStorage when addto-watchlist div is clicked
+      document
+        .querySelector("#search-results")
+        .addEventListener("click", (event) => {
+          if (
+            event.target.matches(".addto-watchlist") ||
+            event.target.closest(".addto-watchlist")
+          ) {
+            localStorage.setItem("poster", data.Poster);
+            localStorage.setItem("title", data.Title);
+            localStorage.setItem(
+              "rating",
+              data.Ratings[0].Value ? data.Ratings[0].Value : "N/A"
+            );
+            localStorage.setItem("runtime", data.Runtime);
+            localStorage.setItem("genre", data.Genre);
+            localStorage.setItem("plot", data.Plot);
+          }
+
+          // Create alert modal here:
+          alert("Movie saved to Watchlist");
+        });
+
+      searchResults.innerHTML = movieCard;
+    })
+    .catch((err) => {
+      searchResults.innerHTML = `<h2 class="error-text">Unable to find what you're looking for. Please try another search.</h2>`;
+    });
 });
--- js/watchlist.js
+++ js/watchlist.js
@@ -1,1 +1,65 @@
 "use strict";
+
+const moviePicks = document.getElementById("movie-picks");
+const movieCard = document.getElementById("movie-card");
+
+const poster = localStorage.getItem("poster");
+const title = localStorage.getItem("title");
+const rating = localStorage.getItem("rating");
+const runtime = localStorage.getItem("runtime");
+const genre = localStorage.getItem("genre");
+const plot = localStorage.getItem("plot");
+
+// if localStorage has a defined poster key, create a movie card
+if (localStorage.getItem("poster") !== null) {
+  moviePicks.innerHTML = `
+  <div class="movie-card" id="movie-card">
+  <div class="poster-container">
+  <img src=${poster} />
+  </div>
+  
+  <div class="info-container">
+  
+  <div class="title-rating-container">
+  <h3>${title}</h3>
+  <h4>&#11088; ${rating}</h4>
+  </div>
+
+  <div class="runtime-genre-container">
+  <h5>${runtime}</h5>
+  <h5>${genre}</h5>
+  <div class="remove-watchlist">
+  <img src="../images/subtract.png" />
+  <h5>Remove</h5>
+  </div>
+  </div>
+  
+  <p class="plot">${plot}</p>
+
+  </div>      
+  `;
+}
+
+document.querySelector("#movie-picks").addEventListener("click", (event) => {
+  if (
+    event.target.matches(".remove-watchlist") ||
+    event.target.closest(".remove-watchlist")
+  ) {
+    localStorage.clear();
+
+    moviePicks.innerHTML = `<h2 class="empty-watchlist">
+                  Your watchlist is looking a little empty...
+                  </h2>
+                  <div class="add-movies">
+                  <img src="../images/add.png" />
+                  <h3><a href="index.html">Let's add some movies!</a></h3>
+                  </div>`;
+  }
+});
+
+//   //     localStorage.removeItem("poster");
+//   //     localStorage.removeItem("title");
+//   //     localStorage.removeItem("rating");
+//   //     localStorage.removeItem("runtime");
+//   //     localStorage.removeItem("genre");
+//   //localStorage.removeItem("plot");
